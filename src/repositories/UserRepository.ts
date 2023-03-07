ea5b7b8504a5837fbd024dcd7b28bc25c01f9b40
unknown <jjoao.monteiro15@gmail.com>
2023-03-06 22:16:04 -0300

    feat: create a interface by user repository

--- src/repositories/UserRepository.ts
+++ src/repositories/UserRepository.ts
@@ -1,8 +1,11 @@
 import { UserData } from "../entities/User/UserEntity"
 import { BaseRepository } from "./helper/MongoHelper"
 
+export interface IUserRepository extends BaseRepository<UserData> {
+    loadByEmail(email: string): Promise<UserData | null>
+}
 
-export class UserRepository extends BaseRepository<UserData> {
+export class UserRepository extends BaseRepository<UserData> implements IUserRepository {
     async loadByEmail(email: string): Promise<UserData | null> {
         const user = await this.collection.findOne({ email })
         return user
