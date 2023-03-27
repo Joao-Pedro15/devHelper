fe1bea3bb443a40e69266ae74637477265e1c578
João Pedro <joao.monteiro@veredaeducacao.com>
2023-03-27 17:27:01 -0300

    refactor: refactoring code errors

--- src/useCases/addUser/AddUserUseCase.ts
+++ src/useCases/addUser/AddUserUseCase.ts
@@ -1,5 +1,8 @@
 import { UserData, UserEntity } from "../../entities/User/UserEntity";
 import { IUserRepository } from "../../repositories/UserRepository";
+import { InternalServerError } from '../../errors/InternalServerError'
+import { AppError } from "../../errors/AppError";
+
 
 export class AddUserUseCase {
   constructor(
@@ -9,12 +12,12 @@ export class AddUserUseCase {
   async execute(data: UserData) {
     try {
       const userEntity = UserEntity.create(data)
-      if(!userEntity) throw new Error('error in validate user entity')
+      if(!userEntity) throw new AppError('error in validate user entity', 400)
       const user = await this.userRepository.create(data)
       return user
     } catch (error) {
-      if(error instanceof Error) throw new Error(error.message)
-      throw new Error('Internal server error')
+      if(error instanceof AppError) throw new AppError(error.message, error.statusCode)
+      throw new InternalServerError()
     }
   }
 
